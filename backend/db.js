f7e7d21ac14b8131ffc1b41294fe5afda60ffaee
Nouman Ali Zaidi <noumanali22222@gmail.com>
2023-05-28 04:52:53 -0700

    installing mongoose and nodemonand also connection our project to database. and creating a simple endpoint.

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+// Connecting to database server
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017";
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+    .then(() => console.log("Connected to Mongo Successfully"))
+    .catch(error => console.log(error));
+}
+
+module.exports = connectToMongo;

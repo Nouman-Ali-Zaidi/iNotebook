7032b31ef13cb5b53e21824efec939419b9d4225
Nouman Ali Zaidi <noumanali22222@gmail.com>
2023-05-28 07:39:26 -0700

    storing author data in database using mongoose model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const obj = {
-        a: "nouman",
-        number: 23
-    }
-    res.send(obj);
+// Create a User using: POST "/api/auth/". Does not require Authorization
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body)
+    user.save()
+    res.send(req.body);
 })
 
 module.exports = router;
